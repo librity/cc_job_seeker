0c9a9588e66842fc5057b35aae5cf10c386e781e
Luis Geniole <luis.geniole@gmail.com>
2020-05-09 22:42:08 -0300

    Add rspec, capybara, rubocop, factorybot and bootstrap.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,18 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+require("jquery");
+require("jquery-mask-plugin");
+
+import "bootstrap";
+import "popper.js";
+
+import "../css/site";
+import "../utils/masks";
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
