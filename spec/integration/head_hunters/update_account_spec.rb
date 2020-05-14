973e55d8edc6e10c6403a875695da40fc1c64ac4
Luis Geniole <luis.geniole@gmail.com>
2020-05-13 23:41:15 -0300

    Style and translate all devise views.

---
+++ spec/integration/head_hunters/update_account_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'Head Hunters can' do
+  scenario 'change their passwords' do
+    head_hunter = log_head_hunter_in!
+
+    visit root_path
+    within 'li#account_dropdown' do
+      click_on I18n.t('views.navigation.change_password')
+    end
+
+    puts head_hunter.password
+    new_password = Faker::Internet.password min_length: 8
+
+    fill_in I18n.t('activerecord.attributes.head_hunter.password'), with: new_password
+    fill_in I18n.t('activerecord.attributes.head_hunter.password_confirmation'), with: new_password
+    fill_in I18n.t('activerecord.attributes.head_hunter.current_password'), with: head_hunter.password
+    within 'form#edit_head_hunter' do
+      click_on I18n.t('views.actions.update')
+    end
+
+    expect(page).to have_content I18n.t('devise.registrations.updated')
+    click_on I18n.t('views.actions.log_out')
+    within 'li#head_hunters_dropdown' do
+      click_on I18n.t('views.navigation.log_in')
+    end
+
+    fill_in I18n.t('activerecord.attributes.head_hunter.email'), with: head_hunter.email
+    fill_in I18n.t('activerecord.attributes.head_hunter.password'), with: new_password
+    within 'form' do
+      click_on I18n.t('views.actions.log_in')
+    end
+
+    expect(page).to have_content I18n.t('devise.sessions.signed_in')
+    expect(current_path).to eq head_hunters_dashboard_path
+  end
+
+  xscenario 'change their profile pictures' do
+  end
+end
---
+++ spec/integration/job_seekers/update_account_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+feature 'Job Seekers' do
+  scenario 'can change their password' do
+    job_seeker = log_job_seeker_in!
+
+    visit root_path
+    within 'li#account_dropdown' do
+      click_on I18n.t('views.navigation.change_password')
+    end
+
+    puts job_seeker.password
+    new_password = Faker::Internet.password min_length: 8
+
+    fill_in I18n.t('activerecord.attributes.job_seeker.password'), with: new_password
+    fill_in I18n.t('activerecord.attributes.job_seeker.password_confirmation'), with: new_password
+    fill_in I18n.t('activerecord.attributes.job_seeker.current_password'), with: job_seeker.password
+    within 'form#edit_job_seeker' do
+      click_on I18n.t('views.actions.update')
+    end
+
+    expect(page).to have_content I18n.t('devise.registrations.updated')
+    click_on I18n.t('views.actions.log_out')
+    within 'li#job_seekers_dropdown' do
+      click_on I18n.t('views.navigation.log_in')
+    end
+
+    fill_in I18n.t('activerecord.attributes.job_seeker.email'), with: job_seeker.email
+    fill_in I18n.t('activerecord.attributes.job_seeker.password'), with: new_password
+    within 'form' do
+      click_on I18n.t('views.actions.log_in')
+    end
+
+    expect(page).to have_content I18n.t('devise.sessions.signed_in')
+    expect(current_path).to eq job_seekers_dashboard_path
+  end
+end
